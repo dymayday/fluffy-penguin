4190aee9ed9868afae3b0e223d627587a7fcf16c
meidhy <dymayday@gmail.com>
2018-07-13 19:49:48 +0200

    Add some header to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! This is the core of our A.I. engine. "EANT2 with prune" is the algorithm we choose to implement and some
+//! documentation can be found in this repository.
+//! We choose EANT2 because of its capability to evolve its own structure and prune the connections
+//! that are unnecessary. I find it very cool and powerful and I invit you to learn more about this
+//! beautiful peace of art =].
+
+
 #[cfg(test)]
 mod tests {
     #[test]
