4ca32149dfac8c00680174cd799a74b7513bd77c
meidhy <dymayday@gmail.com>
2018-07-16 18:10:35 +0200

    Add network encoding file: cge.rs

---
+++ src/cge.rs
@@ -0,0 +1,2 @@
+//! EANT2 represents neural networks and their parameters in a compact way, the Compact Genetic Encoding aka the "linear genome".
+//! It encodes the topology of the network implicitly by the order of its elements (genes).
