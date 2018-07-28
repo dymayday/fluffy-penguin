5e37f553035a7fe9eef17e243334e7031801f8ee
med dem <dymayday@gmail.com>
2018-07-28 22:14:23 +0200

    Move cge in its owned submodule

---
+++ src/cge/mod.rs
@@ -0,0 +1,4 @@
+//! EANT2 represents neural networks and their parameters in a compact way, the Compact Genetic Encoding aka the "linear genome".
+//! It encodes the topology of the network implicitly by the order of its elements (genes).
+
+pub mod node;
