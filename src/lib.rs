d5be3eeb726ca5d94c44f93e293524c8acb31af3
med dem <dymayday@gmail.com>
2018-07-30 12:02:11 +0200

    Add some Node init tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 //! that are unnecessary. I find it very cool and powerful and I invit you to learn more about this
 //! beautiful peace of art =].
 
+extern crate rand;
 pub mod activation;
 pub mod cge;
 
@@ -14,13 +15,36 @@ mod activation_tests {
 
     #[test]
     fn activation_test_relu() {
-        assert_eq!(0.5703423, activation::relu(1_f32));
+        assert_eq!(0.5703423, activation::relu(0.5703423_f32));
+        assert_eq!(0_f32, activation::relu(-10.5703423_f32));
     }
 
 
     #[test]
     fn activation_test_sigmoids() {
-        assert_eq!(0.7310586, activation::sigmoids(1_f32));
+        assert_eq!(0.7310586_f32, activation::sigmoids(1_f32));
     }
-    
+
+}
+
+
+#[cfg(test)]
+mod network {
+    use cge::network::Network;
+
+    #[test]
+    fn partial_eq() {
+        assert_eq!(Network::new(16_i32, 9_i32), Network::new(16_i32, 9_i32));
+    }
+}
+
+
+#[cfg(test)]
+mod node_tests {
+    use cge::node;
+
+    #[test]
+    fn neuron() {
+        node::Neuron::new(0 as usize, 0.3_f32, 1_i32);
+    }
 }
