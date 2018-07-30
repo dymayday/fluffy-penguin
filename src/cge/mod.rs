8c7e2d1e746a9d1a56ecd38e3695532450a450e3
med dem <dymayday@gmail.com>
2018-07-30 12:05:01 +0200

    Add documentation

--- src/cge/mod.rs
+++ src/cge/mod.rs
@@ -1,4 +1,11 @@
-//! EANT2 represents neural networks and their parameters in a compact way, the Compact Genetic Encoding aka the "linear genome".
+//! EANT2 represents neural networks and their parameters in a compact way, the Compact Genetic Encoding (cge) aka the "linear genome".
 //! It encodes the topology of the network implicitly by the order of its elements (genes).
+//!
+//! In CGE, a genotype consists of a string of genes, whose order implicitly represents the
+//! topology of a network. Each gene takes on a specific form (allele): it can either be a vertex
+//! gene (Neuron), an input gene, or a jumper gene. A vertex gene encodes a vertex of a network,
+//! an input gene encodes an input to the network (for example, a sensory signal), and
+//! a jumper gene encodes a connection between two vertices.
 
+pub mod network;
 pub mod node;
