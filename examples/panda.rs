23c9d99ff318371a019c3a35278d9a0bc79d2748
meidhy <dymayday@gmail.com>
2018-08-01 14:38:02 +0200

    Remove useless commented code and add simple init method

--- examples/panda.rs
+++ examples/panda.rs
@@ -6,29 +6,10 @@ use fluffy_penguin::cge::network::Network;
 // use fluffy_penguin::cge::node::Allele;
 
 fn main() {
-    // for i in 0..5 {
-    //     let i: f32 = i as f32 - 2.5_f32;
-    //     // println!("{:4} : ReLu = {} - SoftPlus = {}", i, activation::relu(i), activation::softplus(i));
-    //
-    //     println!("{:4} :", i);
-    //     println!("     ReLu = {}", activation::relu(i));
-    //     // println!("ISRLU 1.0 = {}", activation::isrlu(i, 1_f32));
-    //     println!("ISRLU 3.0 = {}", activation::isrlu(i, 3_f32));
-    //     println!("ISRLU f64 3.0 = {}", (i as f64).isrlu(3_f64));
-    //     // println!(" SoftPlus = {}", activation::softplus(i));
-    //     // println!(" ISRU 1.0 = {}", activation::isru(i, 1_f32));
-    //     // println!(" ISRU 3.0 = {}", activation::isru(i, 3_f32));
-    //     // println!(" Sigmoids = {}", activation::sigmoids(i));
-    //     println!("");
-    // }
-    //
-    //
-    // let n = cge::node::Neuron::new(0 as usize, 0.3_f32, 1_i32);
-    // println!("Neuron = {:#?}", n);
-
-
     let mut network: Network<f32> = Network::build_from_example();
-    println!("Example Node network :");
-    println!("Evaluated output = {:?}", network.evaluate());
+    println!("Evaluated example output = {:?}", network.evaluate());
+
+    let mut panda_net: Network<f32> = Network::new_simple(&vec![1_f32; 16], 9);
+    println!("Evaluated panda_net output = {:?}", panda_net.evaluate());
 
 }
