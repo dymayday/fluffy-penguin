60f98a1375828b59e6bff063b76591e5d2c0ebb0
meidhy <meidhy.demagny@gmail.com>
2018-12-20 18:08:00 +0100

    :white_check_mark: Improve Population's test coverage

--- tests/population.rs
+++ tests/population.rs
@@ -9,9 +9,26 @@ mod population {
 
     #[test]
     fn init_population() {
-        Population::new(10, 2, 1, 0.10)
-            .set_lambda(10)
-            .set_s_rank(1.5);
+        let population_size = 10;
+        let input_size = 2;
+        let output_size = 1;
+
+        // Create a new population with 10 individuals with 2 inputs, 1 output and a mutation
+        // probability of 10%.
+        let mut population = Population::new(population_size, 2, 1, 0.10);
+        // Next we set the 'lambda' attribute to a proper value, usually between half the
+        // population size and half of it.
+        population.set_lambda(10);
+        // And finally we set the 's rank' attribute.
+        population.set_s_rank(1.5);
+
+        let species_size = population.species.len();
+        assert_eq!(species_size, population_size);
+
+        for specimen in population.species.iter() {
+            assert_eq!(input_size, specimen.input_size);
+            assert_eq!(output_size, specimen.output_size);
+        }
     }
 
 
@@ -25,7 +42,7 @@ mod population {
         let population_size: usize = 10;
         let mut population = Population::new(population_size, 2, 1, 0.10);
 
-        // Build some random inputs to feedd the model with.
+        // Build some random inputs to feed the model with.
         let inputs: Vec<[f32; 2]> = (0..population_size)
             .map(|_| {
                 [
