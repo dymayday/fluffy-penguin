cdd0fb51c503c08c0c533cbe657029534eeaa807
meidhy <meidhy.demagny@gmail.com>
2018-12-20 18:07:28 +0100

    :white_check_mark: Add Individual test suite

---
+++ tests/individual.rs
@@ -0,0 +1,30 @@
+//! Individuals tests
+
+extern crate fluffy_penguin;
+
+#[cfg(test)]
+mod individual {
+    use fluffy_penguin::genetic_algorithm::Specimen;
+    use fluffy_penguin::cge::Network;
+
+    #[test]
+    fn crossover() {
+        // Let's build the 2 Specimen from 
+        let spe1 = Specimen {
+            input_size: 1_usize,
+            output_size: 2_usize,
+            ann: Network::<f32>::_build_parent1_from_example(),
+            fitness: 1.0,
+        };
+
+        let spe2 = Specimen {
+            input_size: 1_usize,
+            output_size: 2_usize,
+            ann: Network::<f32>::_build_parent2_from_example(),
+            fitness: 0.0,
+        };
+
+        let child = Specimen::crossover(&spe1, &spe2);
+
+    }
+}
